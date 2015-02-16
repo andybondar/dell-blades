7f0d1fad4567292fe21ef11eb1b615ef0a4642dd
Andrii Bondar <abondar@mirantis.com>
2015-02-16 16:34:00 +0200

    Started implementing 'pexpect' module

--- serveraction.py
+++ serveraction.py
@@ -20,39 +20,58 @@ mask='192.168.220.'
 # Functions
 
 def convert_actions(c):
-    return {
-'stop': 'powerdown',
-'start': 'powerup',
-'reset': 'hardreset',
-'status': 'powerstatus',
-    }.get(c, 'none')
+ return {
+  'stop': 'powerdown',
+  'start': 'powerup',
+  'reset': 'hardreset',
+  'status': 'powerstatus',
+ }.get(c, 'none')
 
 
 
 def single_node(a,n):
-    print "Single node.\n"
-    from subprocess import call
-    url = 'root@' + mask + n
-    cmd = call(["ssh", url, "racadm", "serveraction", a])
-    return
+ print "Single node.\n"
+ ip = mask + n
+ ssh_cmd(ip,'root','root',a)
+ return
 
 def all_nodes(a):
-    print "All nodes.\n"
-    from subprocess import call
-    for i in range(6):
-url = 'root@' + mask + `i+1`
-cmd = call(["ssh", url, "racadm", "serveraction", a])
-    return
+ print "All nodes.\n"
+ for i in range(5):
+  ip = mask + `i+1`
+  ssh_cmd(ip,'root','root',a)
+ return
 
+###
+def ssh_cmd(ip,usr,pw,command):
+ print "Blade ",ip,",",command,"\n" 
+ cmd = 'ssh ' + usr + '@' + ip + ' racadm serveraction ' + command
+ import pexpect
+ ssh_newkey = 'Are you sure you want to continue connecting'
+ # my ssh command line
+ p=pexpect.spawn(cmd)
+ i=p.expect([ssh_newkey,'password:',pexpect.EOF])
+ if i==0:
+  print "I say yes"
+  p.sendline('yes')
+  i=p.expect([ssh_newkey,'password:',pexpect.EOF])
+ if i==1:
+  print "I give password",
+  p.sendline(pw)
+  p.expect(pexpect.EOF)
+ elif i==2:
+  print "I either got key or connection timeout"
+  pass
+ print p.before # print out the result
+ return
 
 ################################
-
 
 import sys
 
 action = convert_actions(sys.argv[1])
 
 if sys.argv[2] == 'all':
-all_nodes(action)
+ all_nodes(action)
 else:
-single_node(action,sys.argv[2])
+ single_node(action,sys.argv[2])
